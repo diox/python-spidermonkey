08131d56d3beef7ee9678424beaed88694a43924
Mathieu Pillard <mpillard@mozilla.com>
2017-03-22 14:07:42 +0000

    Fix tests for python 3, making them expect bytes output/input

--- tests/test_spidermonkey.py
+++ tests/test_spidermonkey.py
@@ -6,10 +6,10 @@ from spidermonkey import Spidermonkey
 def test_command_line_code():
     """Test that command line code is executed."""
 
-    proc = Spidermonkey(code='print("Hello")')
+    proc = Spidermonkey(code=b'print("Hello")')
     stdout, stderr = proc.communicate()
 
-    assert (stdout, stderr) == ('Hello\n', '')
+    assert (stdout, stderr) == (b'Hello\n', b'')
     assert proc.returncode == 0
 
 
@@ -20,9 +20,9 @@ def test_early_script_file(file):
     """Test that an early script file is executed as expected."""
 
     proc = Spidermonkey(early_script_file=file)
-    stdout, stderr = proc.communicate('print("World")')
+    stdout, stderr = proc.communicate(b'print("World")')
 
-    assert (stdout, stderr) == ('World\n', '')
+    assert (stdout, stderr) == (b'World\n', b'')
     assert proc.returncode == 0
 
 
@@ -30,46 +30,46 @@ def test_script_file():
     """Test that a script file is executed as expected."""
 
     proc = Spidermonkey(script_file='-')
-    stdout, stderr = proc.communicate('print("World")')
+    stdout, stderr = proc.communicate(b'print("World")')
 
-    assert (stdout, stderr) == ('World\n', '')
+    assert (stdout, stderr) == (b'World\n', b'')
     assert proc.returncode == 0
 
 
 def test_multi_scripts():
     """Test that when multiple scripts are passed, they're all executed."""
 
-    CODE = ('print(version())',
-            'version(185)',
-            'print(version())',
-            'print("Hello")')
+    CODE = (b'print(version())',
+            b'version(185)',
+            b'print(version())',
+            b'print("Hello")')
 
     proc = Spidermonkey(early_script_file='-', code=CODE)
-    stdout, stderr = proc.communicate('print("World")')
+    stdout, stderr = proc.communicate(b'print("World")')
 
-    assert (stdout, stderr) == ('0\n185\nHello\nWorld\n', '')
+    assert (stdout, stderr) == (b'0\n185\nHello\nWorld\n', b'')
     assert proc.returncode == 0
 
 
 def test_script_args():
     """Test that script args are set as expected."""
 
-    proc = Spidermonkey(code='print(scriptArgs)',
-                        script_args=('Hello', 'World'))
+    proc = Spidermonkey(code=b'print(scriptArgs)',
+                        script_args=(b'Hello', b'World'))
     stdout, stderr = proc.communicate('')
 
-    assert (stdout, stderr) == ('Hello,World\n', '')
+    assert (stdout, stderr) == (b'Hello,World\n', b'')
     assert proc.returncode == 0
 
 
 def test_extra_flags():
     """Test that extra flags are processed as expected."""
 
-    proc = Spidermonkey(extra_flags=('-e', 'print(scriptArgs)'),
-                        script_args=('Hello', 'World'))
+    proc = Spidermonkey(extra_flags=('-e', b'print(scriptArgs)'),
+                        script_args=(b'Hello', b'World'))
     stdout, stderr = proc.communicate('')
 
-    assert (stdout, stderr) == ('Hello,World\n', '')
+    assert (stdout, stderr) == (b'Hello,World\n', b'')
     assert proc.returncode == 0
 
 
@@ -78,7 +78,7 @@ def test_compileonly():
 
     # Valid code: no output, returns 0.
     proc = Spidermonkey(compile_only=True, script_file='-')
-    stdout, stderr = proc.communicate('print("Hello")')
+    stdout, stderr = proc.communicate(b'print("Hello")')
 
     assert not stdout
     assert not stderr
@@ -86,13 +86,13 @@ def test_compileonly():
 
     # Invalid code: only stderr output, returns > 0.
     proc = Spidermonkey(compile_only=True, script_file='-')
-    stdout, stderr = proc.communicate('print("Hello"')
+    stdout, stderr = proc.communicate(b'print("Hello"')
 
-    assert stdout == ''
-    assert stderr != ''
+    assert stdout == b''
+    assert stderr != b''
     assert proc.returncode > 0
 
     # The compile-only flag is not applied to code passed on the
     # command line, so it should not be accepted.
     with pytest.raises(AssertionError):
-        Spidermonkey(compile_only=True, code="print('Hello')")
+        Spidermonkey(compile_only=True, code=b"print('Hello')")
